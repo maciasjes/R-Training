443b8fd65fff8c60f20f35414b56bae474e79cd1
Jessica Macias <maciasjes@paho.org>
2024-10-28 15:57:05 -0400

    Unit3 finished

--- Exercise1.R
+++ Exercise1.R
@@ -93,3 +93,55 @@ dat <- read_excel("Datos/datos_covid.xlsx")
 
 #Funciones básicas de tydyverse
 dat %>% glimpse()
+
+#Cargar datos
+url <- "https://github.com/TRACE-LAC/TRACE-LAC-data/raw/refs/heads/main/otros/datos_limpios_covid.RDS"
+
+covid19 <- readr::read_rds(url)
+
+#Explorar los datos
+glimpse (covid19)
+
+#Agrupar los datos por nombre_del_pais y cuenta los casos por cada uno
+covid19 %>% group_by(nombre_del_pais) %>% 
+  summarise(registros = n())
+
+#Filtra los datos para Perú 
+covidperu <- covid19 %>% filter(nombre_del_pais == "Perú") %>%
+  glimpse()
+
+#Agrupa los datos por sexo y cuenta los casos por cada uno
+covid19 %>% group_by(sexo) %>% 
+  summarise(registros = n())
+
+#Agrupa los datos por nombre_del_pais y calcula la media de edad de cada uno
+covid19 %>% group_by(nombre_del_pais) %>% 
+  summarise(media_edad = mean(edad))
+
+#Cambia el nombre de “estado” por “gravedad”
+covid19 %>% rename(gravedad = estado) %>% 
+  glimpse ()
+
+#Ordena los datos por sexo y selecciona las 5 primeras filas de cada uno
+covid19 %>% arrange(sexo) %>%
+  group_by(sexo) %>%
+  slice (1:5)
+
+#Realiza una tabla que muestre cuántas personas de cada sintoma aparecen en la base ubicados en Colombia
+covid19 %>% group_by(sintomas) %>%
+  filter(nombre_del_pais == "Colombia") %>%
+  summarise(casos = n())
+
+#Selecciona las 5 primeras filas de solo el número de identificación del caso (id_de_caso)
+covid19 %>% select(id_de_caso) %>%
+  slice(1:5)
+
+#¿Cuál es el país con mayor número de fallecidos de acuerdo con la variable estado?
+covid19 %>% filter(estado == "Fallecido") %>%
+  group_by(nombre_del_pais) %>%
+  summarise(casos = n())
+
+#¿Cuál es el país que presenta la mayor edad media en sus registros?
+covid19 %>% group_by(nombre_del_pais) %>%
+  summarise(media_edad = mean(edad)) %>%
+  arrange(desc(media_edad))
